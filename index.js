f469c792f1d597b16c5b6d9969530b68a83b3bec
Harsimran Singh <harsimran0692@gmail.com>
2024-11-23 11:13:05 -0500

    test change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-    res.send("Hello Azure");
+    res.send("Hello Azure")
 });
 app.get("/test", (req, res) => {
     res.send("Hello Azure Test");
